62ab3f3d6d48b09af7319df0e3cf39446b47b82f
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-17 17:08:43 +0530

    fix bug

--- lib/passwordstrenghchecker/widgets/methods/passwordstrengthresult.dart
+++ lib/passwordstrenghchecker/widgets/methods/passwordstrengthresult.dart
@@ -1,42 +1,61 @@
-// lib/src/utils/password_strength_calculator.dart
-
 class PasswordStrengthResult {
   final double strength;
   final String text;
 
-  PasswordStrengthResult({
+  final bool hasMinLength;
+  final bool hasUpperCase;
+  final bool hasLowerCase;
+  final bool hasNumber;
+  final bool hasSpecialChar;
+
+  const PasswordStrengthResult({
     required this.strength,
     required this.text,
+    required this.hasMinLength,
+    required this.hasUpperCase,
+    required this.hasLowerCase,
+    required this.hasNumber,
+    required this.hasSpecialChar,
   });
 }
 
 PasswordStrengthResult calculatePasswordStrength(
     String password,
     ) {
+  final hasMinLength = password.length >= 8;
+  final hasUpperCase = RegExp(r'[A-Z]').hasMatch(password);
+  final hasLowerCase = RegExp(r'[a-z]').hasMatch(password);
+  final hasNumber = RegExp(r'[0-9]').hasMatch(password);
+  final hasSpecialChar =
+  RegExp(r'[!@#\$%^&*(),.?":{}|<>]').hasMatch(password);
+
   int score = 0;
 
-  if (password.length >= 8) score++;
-  if (RegExp(r'[A-Z]').hasMatch(password)) score++;
-  if (RegExp(r'[a-z]').hasMatch(password)) score++;
-  if (RegExp(r'[0-9]').hasMatch(password)) score++;
-  if (RegExp(r'[!@#\$%^&*(),.?":{}|<>]').hasMatch(password)) score++;
+  if (hasMinLength) score++;
+  if (hasUpperCase) score++;
+  if (hasLowerCase) score++;
+  if (hasNumber) score++;
+  if (hasSpecialChar) score++;
 
-  if (score <= 2) {
-    return PasswordStrengthResult(
-      strength: score / 5,
-      text: 'Weak',
-    );
-  }
+  String text;
 
-  if (score <= 4) {
-    return PasswordStrengthResult(
-      strength: score / 5,
-      text: 'Medium',
-    );
+  if (password.isEmpty) {
+    text = 'Enter Password';
+  } else if (score <= 2) {
+    text = 'Weak';
+  } else if (score <= 4) {
+    text = 'Medium';
+  } else {
+    text = 'Strong';
   }
 
   return PasswordStrengthResult(
     strength: score / 5,
-    text: 'Strong',
+    text: text,
+    hasMinLength: hasMinLength,
+    hasUpperCase: hasUpperCase,
+    hasLowerCase: hasLowerCase,
+    hasNumber: hasNumber,
+    hasSpecialChar: hasSpecialChar,
   );
 }
