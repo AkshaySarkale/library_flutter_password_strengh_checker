908cc0510cae0fd6550dc7bd8c64db1440cd8438
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-17 17:14:10 +0530

    added libraries

--- lib/library_flutter_password_strengthchecker.dart
+++ lib/library_flutter_password_strengthchecker.dart
@@ -1,5 +1,13 @@
+library library_flutter_password_strengthchecker;
+export 'passwordstrenghchecker/widgets/passwordmeter.dart';
+export 'passwordstrenghchecker/widgets/requirements.dart';
+export 'passwordstrenghchecker/widgets/methods/passwordstrengthresult.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+// lib/library_flutter_password_strengthchecker.dart
+
+
